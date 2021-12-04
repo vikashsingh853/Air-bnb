bc1b3faa24061d5fdfc52d4700492a03351936c3
V1singham <44531543+V1singham@users.noreply.github.com>
2021-12-04 16:42:48 +0530

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
     return (
         <div className='footer'>
-            <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+            <p>© 2021 Airbnb clone! No rights reserved - this is a demo!</p>
             <p>Privacy · Terms · Sitemap · Company Details</p>
         </div>
     )
